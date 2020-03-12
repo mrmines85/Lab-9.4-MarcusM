8dfb59b21979b0ac7ea5e78936281d8861297700
mrmines85 <61034810+mrmines85@users.noreply.github.com>
2020-03-12 17:54:58 -0500

    Add files via upload
    Lab 9.4 MarcusM

---
+++ ConsoleApp20/ConsoleApp20/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+using System.Text;
+
+
+namespace ConsoleApp20
+{
+    class HTMLHeader
+    {
+        public const string open = "<h1>";
+        public const string close = "</h1>";
+
+        public string CreateHeader(string text)
+        {
+            string header = String.Concat(open, text, close, "\n");
+            return header;
+        }
+    }
+
+    class UnorderedList
+    {
+        public const string open = "<ul>";
+        public const string close = "</ul>";
+
+        public string CreateListItem(string text)
+        {
+            string open = "<li>\n";
+            string close = "</li>\n";
+
+            string listItem = String.Concat(open, text, close, "\n");
+            return listItem;
+        }
+
+        public StringBuilder CreateList(string[] listItems)
+        {
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(open);
+            foreach (string item in listItems)
+            {
+                sb.Append(item);
+            }
+            sb.Append(close);
+
+            return sb;
+        }
+
+    }
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "D:/Users/mmines/source/repos/ConsoleApp20/Weblogs/Lab9_4.html";
+            HTMLHeader header = new HTMLHeader();
+            UnorderedList list = new UnorderedList();
+            StringBuilder sb = new StringBuilder();
+
+            Console.WriteLine("Enter text for HTML header.");
+            string headerElement = header.CreateHeader(Console.ReadLine());
+            string[] listItems = new string[3];
+
+            for (int i = 0; i < listItems.Length; i++)
+            {
+                Console.WriteLine("Add another item to the list.");
+                listItems[i] = list.CreateListItem(Console.ReadLine());
+            }
+
+            StringBuilder listElement = list.CreateList(listItems);
+            sb.Append(headerElement);
+            sb.Append(listElement.ToString());
+
+            File.AppendAllText(filename, sb.ToString());
+
+        }
+    }
+}
